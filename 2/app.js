a75d5846815168d1b742a9c415a010be6b621914
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-26 18:24:21 +0900

    変数2

--- 2/app.js
+++ 2/app.js
@@ -3,3 +3,17 @@
 
 var hello = "Hallo, World";
 alert(hello);
+
+// 変数には、任意の数値、文字列、関数、も代入出来る。
+
+// 整数を代入
+var int1 = 1;
+
+// 負数を代入
+var int2 = -10;
+
+// 小数点を代入
+var float = 3.14;
+
+// 文字列を代入
+var str1 = 'JavaScriptを覚えよう';
