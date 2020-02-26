28caadac70619b70ae549e4a34cf2d9e61873b1e
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-26 20:50:43 +0900

    繰り返し処理do...while

--- 2/app.js
+++ 2/app.js
@@ -1,16 +1,5 @@
-// while文では、条件がtrueである間は繰り返し処理が実行
-// 引数の条件がtrueの間、波括弧内部の処理が繰り返される
-// while(条件){
-	// 処理
-// }
+// do...while文は、条件式のtrue/falseに関わらず、最初の１回だけは必ず処理
 
-var max = 100;
-var num = 1;
-var count = 0;
-
-while(num < max){
-	num = num * 2;
-	count = count + 1;
-}
-
-alert('２を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+do{
+	最低１回は行われる処理
+}while(条件式);
